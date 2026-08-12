b358c1ea2b276f2ca4d709195b95704ebde250b0
gael-bit <gael.ndibodjo@gmail.com>
2026-08-12 20:06:16 +0200

    test(backend): use PostgreSQL testcontainer

--- backend/src/test/java/com/teachandcorrect/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/teachandcorrect/backend/BackendApplicationTests.java
@@ -2,12 +2,31 @@ package com.teachandcorrect.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("teachandcorrect_test")
+            .withUsername("admin")
+            .withPassword("admin");
+
+    @DynamicPropertySource
+    static void configureDatasource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
